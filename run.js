e6f222b38f3436c88ed3519218d8509310dd722e
Jared Forsyth <jared@jaredforsyth.com>
2014-09-06 20:58:03 -0600

    stdin, out, errors. all working

--- run.js
+++ run.js
@@ -2,10 +2,9 @@
  * bootstrap the app into document.body
  */
 
-var App = require('./lib/app')
 var Kernel = require('./lib/kernel')
-var React = require('react/addons')
-var treed = require('../treed/rx')
+var React = require('treed/node_modules/react')
+var treed = require('treed/rx')
 var convert = require('./lib/convert')
 
 window.React = React
@@ -14,22 +13,26 @@ window.onload = function () {
   var host = "localhost:8889"
     , path = "test.ipynb"
     , kernel = new Kernel(host, path)
-    
+
   kernel.init((notebook_data) => {
-    /*(
-    React.renderComponent(App({
-      kernel: kernel,
-    }), document.body)
-    */
     var data = convert.fromNotebook(notebook_data.content.worksheets[0].cells)
     treed.quickstart(document.body, {
       storeOptions: {data: data},
+      React: React,
+      plugins: [
+        require('./lib/treed-plugin'),
+        require('treed/rx/plugins/undo'),
+        require('treed/rx/plugins/tags'),
+        require('treed/rx/plugins/rebase'),
+        require('treed/rx/plugins/collapse'),
+        require('treed/rx/plugins/done'),
+      ],
     }, (store) => {
+      store._globals.kernel = kernel
       window.store = store
       window.actions = store.actions
     })
   })
-
 
 }
 
