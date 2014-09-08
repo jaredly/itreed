e347a0c378b58d084d58e7e3770127adb47c1c7f
Jared Forsyth <jared@jaredforsyth.com>
2014-09-07 22:50:22 -0600

    full rendering of gorilla list-likes

--- run.js
+++ run.js
@@ -6,32 +6,39 @@ var Gorilla = require('./lib/Gorilla')
 var Kernel = require('./lib/kernel')
 var React = require('treed/node_modules/react')
 var treed = require('treed/rx')
+var LocalPL = require('treed/rx/pl/local')
 var convert = require('./lib/convert')
 
 window.React = React
 
 window.onload = function () {
   // var host = "localhost:8889"
-  var host = "localhost:60830"
+  var host = "localhost:" + location.hash.split('/')[0].slice(1)
     , path = "test.ipynb"
     , kernel = new Gorilla(host, path)
 
-  kernel.init((notebook_data) => {
-    var data = convert.fromNotebook(notebook_data.content.worksheets[0].cells)
-    treed.quickstart(document.body, {
-      storeOptions: {data: data},
-      React: React,
-      plugins: [
-        require('./lib/treed-plugin'),
-        require('treed/rx/plugins/undo'),
-        require('treed/rx/plugins/rebase'),
-        require('treed/rx/plugins/collapse'),
-        require('treed/rx/plugins/done'),
-      ],
-    }, (store) => {
-      store._globals.kernel = kernel
-      window.store = store
-      window.actions = store.actions
+  var data = convert.fromNotebook([
+      {cell_type: 'markdown', source: '# Ok who is awesome?'},
+      {cell_type: 'code', language: 'clojure', input: '(+ 2 3)', outputs: []},
+  ])
+  treed.quickstart(document.body, {
+    storeOptions: {
+      data: data,
+      pl: new LocalPL({prefix: 'ipython'}),
+    },
+    React: React,
+    plugins: [
+      require('./lib/treed-plugin'),
+      require('treed/rx/plugins/undo'),
+      require('treed/rx/plugins/rebase'),
+      require('treed/rx/plugins/collapse'),
+      require('treed/rx/plugins/done'),
+    ],
+  }, (store) => {
+    window.store = store
+    window.actions = store.actions
+    store._globals.kernel = kernel
+    kernel.init(() => {
     })
   })
 
