2c0f4f9dd5dbb22548fbaac1d8b748cd2bb0a601
Jared Forsyth <jared@jaredforsyth.com>
2014-10-04 08:35:11 -0600

    python magic

---
+++ itreed/load_magic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from magic import MyMagics
+# In order to actually use these magics, you must register them with a
+# running IPython.  This code must be placed in a file that is loaded once
+# IPython is up and running:
+ip = get_ipython()
+# You can register the class itself without instantiating it.  IPython will
+# call the default constructor on it.
+ip.register_magics(MyMagics)
+
+
+# vim: et sw=4 sts=4
---
+++ itreed/modules.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from IPython.core.magic import (Magics, magics_class, line_magic,
+                                cell_magic, line_cell_magic)
+import json
+
+@magics_class
+class Magics(Magics):
+
+    def __init__(self, *a, **k):
+        Magics.__init__(self, *a, **k)
+
+        self.modules = {}
+
+    def get_namespaces(self, namespaces):
+        globz = self.shell.user_global_ns.copy()
+        last = namespaces.pop(-1)
+        ns = self.shell.user_ns.get('modules', {})
+        for name in namespaces:
+            if name not in ns:
+                ns[name] = {}
+            ns = ns[name]
+            for sub in ns:
+                globz[sub] = ns[sub]
+        if last not in ns:
+            ns[last] = {}
+        local = ns[last]
+        return globz, local
+
+    @cell_magic
+    def cmagic(self, line, cell):
+        try:
+            items = json.parse(line)
+        except:
+            items = [line]
+        globz, local = self.get_namespaces(items)
+        result = execute(cell, '<cell>', globz, local)
+        return line, cell
+
+    @line_cell_magic
+    def lcmagic(self, line, cell=None):
+        "Magic that works both as %lcmagic and as %%lcmagic"
+        if cell is None:
+            print("Called as line magic")
+            return line
+        else:
+            print("Called as cell magic")
+            return line, cell
+
+    '''
+    @line_magic
+    def lmagic(self, line):
+        "my line magic"
+        print("Full access to the main IPython object:", self.shell)
+        print("Variables in the user namespace:", list(self.shell.user_ns.keys()))
+        return line
+    '''
+
+def execute(code, filename, global_ns, local_ns)
+    try:
+        tree = compile(code, filename, 'exec', ast.PyCF_ONLY_AST)
+    except Exception as e:
+        print "Syntax", e
+        return e
+    expressions = tree.body
+    results = []
+    try:
+        for item in tree.body:
+            if isinstance(item, ast.Expr):
+                code = compile(ast.Expressions(item.value, lineno=item.lineno, col_offset=item.col_offset), filename, 'eval')
+                results.append(eval(code, global_ns, local_ns))
+            else:
+                code = compile(ast.Module([item]), filename, 'exec')
+                exec(code, global_ns, local_ns)
+    except Exception as e:
+        print "Failure!", e
+        return e
+    return results
+
+
+# vim: et sw=4 sts=4
