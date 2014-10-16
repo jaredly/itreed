2ce0b34bcca928940a95961611be499909d63a1b
Jared Forsyth <jared@jaredforsyth.com>
2014-10-16 15:29:57 -0600

    webworker context going well

--- www/bootworker.js
+++ www/bootworker.js
@@ -7,11 +7,37 @@ function send(name, data) {
 
 var HANDLERS = {
   'eval': function (payload) {
-    send({
-      id: payload.id,
-      value: eval(payload.text)
-    })
+    var res, err
+    try {
+      res = eval(payload.text)
+    } catch (e) {
+      err = e
+    }
+    send('eval', {id: payload.id, error: err, result: res})
   },
+  'go': function (payload) {
+    var fn, err
+    try {
+      eval('fn = ' + payload.what)
+    } catch (e) {
+      console.log('er!', e)
+      console.log(e)
+      console.log(payload)
+      return send('go', {id: payload.id, args: [e]})
+    }
+    try {
+      fn(function () {
+        send('go', {
+          id: payload.id,
+          args: [null].concat([].slice.call(arguments))
+        })
+      })
+    } catch (e) {
+      console.log('er!', e)
+      console.log(e)
+      send('go', {id: payload.id, args: [e]})
+    }
+  }
 }
 
 onmessage = function (evt) {
