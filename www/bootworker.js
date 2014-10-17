7ab60531bb0f231bab7d3c2a997e116e092fa212
Jared Forsyth <jared@jaredforsyth.com>
2014-10-17 07:53:42 -0600

    lots of WIP with js sandboxing, webworkers

--- www/bootworker.js
+++ www/bootworker.js
@@ -5,16 +5,25 @@ function send(name, data) {
   postMessage({name: name, data: data})
 }
 
+function cloneE(e) {
+  return {
+    name: e.name,
+    message: e.message,
+    stack: e.stack,
+  }
+}
+
 var HANDLERS = {
   'eval': function (payload) {
     var res, err
     try {
       res = eval(payload.text)
     } catch (e) {
-      err = e
+      err = cloneE(e)
     }
     send('eval', {id: payload.id, error: err, result: res})
   },
+
   'go': function (payload) {
     var fn, err
     try {
@@ -23,7 +32,7 @@ var HANDLERS = {
       console.log('er!', e)
       console.log(e)
       console.log(payload)
-      return send('go', {id: payload.id, args: [e]})
+      return send('go', {id: payload.id, error: cloneE(e)})
     }
     try {
       fn(function () {
@@ -35,7 +44,7 @@ var HANDLERS = {
     } catch (e) {
       console.log('er!', e)
       console.log(e)
-      send('go', {id: payload.id, args: [e]})
+      send('go', {id: payload.id, error: cloneE(e)})
     }
   }
 }
