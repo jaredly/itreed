f0223d5a730ba37cc650027cf43c4cbfb5b1a9e9
Jared Forsyth <jared@jaredforsyth.com>
2014-11-09 00:04:17 -0700

    various fixes related to getting swarmjs example working!!

---
+++ formatters/dom.js
@@ -0,0 +1,47 @@
+
+var React = require('treed/node_modules/react')
+  , uuid = require('../lib/uuid')
+
+  , _objs = {}
+
+module.exports = {
+  mime: 'js/dom',
+
+  format: function (obj, window) {
+    if (obj instanceof window.HTMLElement) {
+      var id = uuid()
+      _objs[id] = obj
+      return id
+    }
+  },
+
+  display: function (id) {
+    if (!_objs[id]) return <em>Re-evaluate this block to see the DOM element</em>
+    return <DomViewer value={_objs[id]}/>
+  },
+}
+
+function clean(node) {
+  while (node.lastChild) node.removeChild(node.lastChild)
+}
+
+var DomViewer = React.createClass({
+  componentDidMount: function () {
+    this.inject()
+  },
+  componentDidUpdate: function (prevProps) {
+    this.inject()
+  },
+  inject: function () {
+    var node = this.getDOMNode()
+    clean(node)
+    //if (this.props.value.parentNode) {
+      //node.innerHTML = '<em>DOM Node already used elsewhere</em>'
+    //} else {
+      node.appendChild(this.props.value)
+    //}
+  },
+  render: function () {
+    return <div/>
+  },
+})
--- run.js
+++ run.js
@@ -20,6 +20,7 @@ var formatters = [
   require('./formatters/live-button'),
   require('./formatters/react'),
   require('./formatters/vega'),
+  require('./formatters/dom'),
   // require('./formatters/image'),
   require('./formatters/list-like'),
   require('./formatters/js'),
